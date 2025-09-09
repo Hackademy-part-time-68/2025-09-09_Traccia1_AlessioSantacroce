e80dfdf1d8ee9f7ce4a6c65a0f9cec29f58f64f7
Alessio Santacroce <alesantacroce23@gmail.com>
2025-09-09 19:06:02 +0200

    aggiunte stringhe

--- index.js
+++ index.js
@@ -9,3 +9,23 @@ var num2;
 num2 = 2;
 
 console.log(num2);
+
+var num1;
+
+num1 = 'Il valore della variabile num1 è 3' ;
+
+console.log(num1);
+
+var num2;
+
+num2 = 'Il valore della variabile num2 è 2' ;
+
+console.log(num2);
+
+var stringa;
+
+stringa = 'il valore della variabile stringa è 4'
+
+console.log(stringa);
+
+
