e40015fa86f368c538e8d58a1d6805617fc72f91
Alessio Santacroce <alesantacroce23@gmail.com>
2025-09-09 19:43:46 +0200

    traccia 2 completata

--- index.js
+++ index.js
@@ -10,13 +10,13 @@ num2 = 2;
 
 console.log(num2);
 
-var num1;
+
 
 num1 = 'Il valore della variabile num1 è 3' ;
 
 console.log(num1);
 
-var num2;
+
 
 num2 = 'Il valore della variabile num2 è 2' ;
 
@@ -32,6 +32,25 @@ console.log(stringa);
 const PIGRECO = 3.14159265359
 
 console.log('il valore della costante PIGRECO è' + PIGRECO);
+
+/// traccia 2 ///
+var stringa2;
+stringa2 = 'traccia2';
+console.log(stringa2);
+
+var a = 1;
+var b = 2;
+var c = 3;
+var d = 4;
+var e = 5;
+ var x = a+b+c+d+e
+console.log(x);
+
+var y
+
+y = x/5
+console.log(y);
+
 
 
 
